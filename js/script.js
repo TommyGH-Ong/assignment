15a048eb5a5dbb2676b226d95a2753a1ac030a51
Tommy <tommy@gmail.com>
2023-04-01 18:15:48 -0700

    Wrote functions to hide and show buttons and textarea

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const buttonRef = document.querySelector("#dark")
+let box = document.querySelector(".textbox")
+let text = document.querySelector("textarea")
 
 function changeText() {
     if (buttonRef.textContent === "Dark Theme"){
@@ -11,8 +13,19 @@ function changeText() {
 function lightsOff() {
     let element = document.body;
     let note = document.querySelector(".notes")
-    let text = document.querySelector("textarea")
     element.classList.toggle("darkerMode");
     note.classList.toggle("darkMode");
     text.classList.toggle("darkMode");
+}
+
+function hideButtons() {
+    box.style.display = "none";
+}
+
+function showButtons() {
+    if (box.style.display === "none") {
+        box.style.display = "block";
+    }else{
+        text.value=""
+    }
 }
